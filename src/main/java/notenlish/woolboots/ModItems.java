dbe9f99a6eb2b2cde9f655c8029a00262c48450b
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-08-15 19:49:50 +0300

    i forgot I dont need to make 3d models for the boots

---
+++ src/main/java/notenlish/woolboots/ModItems.java
@@ -0,0 +1,41 @@
+package notenlish.woolboots;
+
+import kotlin.Unit;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Unbreakable;
+
+public class ModItems {
+	public static void initialize() {
+		// Get the event for modifying entries in the ingredients group.
+// And register an event handler that adds our suspicious item to the ingredients group.
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT)
+				.register((itemGroup) -> itemGroup.accept(ModItems.WHITE_WOOL_BOOTS));
+	}
+
+	public static Item register(Item item, String id) {
+		// Create the identifier for the item.
+		ResourceLocation itemID = ResourceLocation.fromNamespaceAndPath(ModTemplate.MOD_ID, id);
+
+		// Register the item.
+		Item registeredItem = Registry.register(BuiltInRegistries.ITEM, itemID, item);
+
+		// Return the registered item!
+		return registeredItem;
+	}
+
+	public static final Item WHITE_WOOL_BOOTS = register(
+			new ArmorItem(
+					ModArmorMaterials.WOOLITE,
+					ArmorItem.Type.BOOTS, new Item.Properties().durability(297)
+			), "white_wool_boots");
+
+
+
+}
--- src/main/java/notenlish/woolboots/ModTemplate.java
+++ src/main/java/notenlish/woolboots/ModTemplate.java
@@ -16,10 +16,9 @@ import notenlish.woolboots.platform.fabric.FabricPlatform;
 
 @SuppressWarnings("LoggingSimilarMessage")
 public class ModTemplate {
-
-	public static final String MOD_ID = /*$ mod_id*/ "modtemplate";
+	public static final String MOD_ID = /*$ mod_id*/ "woolboots";
 	public static final String MOD_VERSION = /*$ mod_version*/ "0.1.0";
-	public static final String MOD_FRIENDLY_NAME = /*$ mod_name*/ "Mod Template";
+	public static final String MOD_FRIENDLY_NAME = /*$ mod_name*/ "Wool Boots";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	private static final Platform PLATFORM = createPlatformInstance();
@@ -27,6 +26,9 @@ public class ModTemplate {
 	public static void onInitialize() {
 		LOGGER.info("Initializing {} on {}", MOD_ID, ModTemplate.xplat().loader());
 		LOGGER.debug("{}: { version: {}; friendly_name: {} }", MOD_ID, MOD_VERSION, MOD_FRIENDLY_NAME);
+
+		ModArmorMaterials.initialize();
+		ModItems.initialize();
 	}
 
 	public static void onInitializeClient() {
